092a6a592cee779aac2b12ebda53c3ac5ab36156
Tran Hong Gam <honggamdx2@gmail.com>
2026-04-17 12:36:25 +0700

    fix(be): delete module review

--- backend/src/modules/review/interfaces/review.service.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface IReviewService {
-  findOne(id: string): Promise<unknown>;
-}
--- backend/src/modules/review/schema/review.schema.ts
+++
@@ -1,7 +0,0 @@
-export type ReviewSchema = {
-  name: string;
-};
-
-export const reviewSchema = {
-  parse: <T>(payload: T) => payload,
-};
