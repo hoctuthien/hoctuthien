33d52512ac2b0a81af07e2a4e89f1cfd95a78c76
Tran Hong Gam <honggamdx2@gmail.com>
2026-04-17 11:14:52 +0700

    feat(be): AuthService

--- backend/src/modules/auth/dtos/auth.dto.ts
+++ backend/src/modules/auth/dtos/auth.dto.ts
@@ -1,7 +1,23 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  IsOptional,
+} from 'class-validator';
+import { v4 as uuidv4 } from 'uuid'; 
 
-export class AuthDto {
+export class LoginDto {
+  @IsEmail({}, { message: 'Email không đúng định dạng' })
+  @IsNotEmpty({ message: 'Email không được để trống' })
+  email!: string;
+
   @IsString()
-  @IsNotEmpty()
-  name: string;
+  @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
+  @MinLength(6, { message: 'Mật khẩu phải ít nhất 6 ký tự' })
+  password!: string;
+
+  @IsString()
+  @IsOptional() // Để là Optional để Server có thể tự điền nếu Client gửi thiếu
+  deviceId?: string;
 }
